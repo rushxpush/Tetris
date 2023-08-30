f653b1a8e237c2d0c12583a5bf4d20061e4551ea
rushxpush <rushxpush@gmail.com>
2023-08-29 23:06:22 -0300

    Created function to initialize tetrominoes

--- Tetris.cpp
+++ Tetris.cpp
@@ -17,57 +17,67 @@ public:
 
 BlocksColor blocks_color[3];
 
+char blocks[6] = { 'T', 'B','L', 'I', 'S', 'Z'};
+
 
 
 
 class Tetromino
 {
 private:
-	const char* shape = "t";
+	char shape = 'T';
 	float x = 1.0f;
 	float y = 1.0f;
 	float z = 1.0f;
 	float width = 1.0f;
 	float height = 1.0f;
 	float length = 1.0f;
-	// float block[4][3] = { {0.0f, 0.0f, -1.0f}, {0.0f, 0.0f, 0.0f} , {0.0f, 0.0f, 1.0f}, {0.0f, -1.0f, 0.0f} };
 
 	struct block_1 {
-		float x = 0.0f;
-		float y = 0.0f;
-		float z = -1.0f;
-		int position = 0;
+		float x;
+		float y;
+		float z;
+		int position;
 	} block_1;
 
 	struct block_2 {
-		float x = 0.0f;
-		float y = 0.0f;
-		float z = 0.0f;
+		float x;
+		float y;
+		float z;
+		int position;
 	} block_2;
 
 	struct block_3 {
-		float x = 0.0f;
-		float y = 0.0f;
-		float z = 1.0f;
-		int position = 2;
+		float x;
+		float y;
+		float z;
+		int position;
 	} block_3;
 
 	struct block_4 {
-		float x = 0.0f;
-		float y = 1.0f;
-		float z = 0.0f;
-		int position = 1;
+		float x;
+		float y;
+		float z;
+		int position;
 	} block_4;
 
 	float t_position[4][3] = { {0.0f, 1.0f, -1.0f}, {0.0f, 1.0f, 1.0f}, {0.0f, -1.0f, 1.0f}, {0.0f, -1.0f, -1.0f} };
+
+	float b_position[4][3] = { {0.0f, 1.0f, -1.0f}, {0.0f, 1.0f, 1.0f}, {0.0f, -1.0f, 1.0f}, {0.0f, -1.0f, -1.0f} };
+	float l_position[4][3] = { {0.0f, 1.0f, -1.0f}, {0.0f, 1.0f, 1.0f}, {0.0f, -1.0f, 1.0f}, {0.0f, -1.0f, -1.0f} };
+	float i_position[4][3] = { {0.0f, 1.0f, -1.0f}, {0.0f, 1.0f, 1.0f}, {0.0f, -1.0f, 1.0f}, {0.0f, -1.0f, -1.0f} };
+	float s_position[4][3] = { {0.0f, 1.0f, -1.0f}, {0.0f, 1.0f, 1.0f}, {0.0f, -1.0f, 1.0f}, {0.0f, -1.0f, -1.0f} };
+	float z_position[4][3] = { {0.0f, 1.0f, -1.0f}, {0.0f, 1.0f, 1.0f}, {0.0f, -1.0f, 1.0f}, {0.0f, -1.0f, -1.0f} };
 
 	Color flat_color = RED;
 	Color edge_color = MAROON;
 
 public:
+	bool tetromino_exist = false;
+
 	void Draw()
 	{
-		if (shape == "t")
+		if (shape == 'T')
 		{
 
 			//  x
@@ -99,6 +109,7 @@ public:
 
 	void Update()
 	{
+
 		// Gravity
 		y = 1.0f;
 		/*
@@ -108,40 +119,11 @@ public:
 		block_4.y += y;
 		*/
 
-		if (IsKeyPressed(KEY_DOWN))
-		{
-
-
-		cout << "---------------------" << endl;
-		cout << "block_1.position: " << block_1.position << endl;
-		cout << "block_1.x: " << block_1.x << endl;
-		cout << "block_1.y: " << block_1.y << endl;
-		cout << "block_1.z: " << block_1.z << endl;
-		cout << " " << endl;
-
-		cout << "block_2.x: " << block_2.x << endl;
-		cout << "block_2.y: " << block_2.y << endl;
-		cout << "block_2.z: " << block_2.z << endl;
-		cout << " " << endl;
-
-		cout << "block_3.position: " << block_3.position << endl;
-		cout << "block_3.x: " << block_3.x << endl;
-		cout << "block_3.y: " << block_3.y << endl;
-		cout << "block_3.z: " << block_3.z << endl;
-		cout << " " << endl;
-
-		cout << "block_4.position: " << block_4.position << endl;
-		cout << "block_4.x: " << block_4.x << endl;
-		cout << "block_4.y: " << block_4.y << endl;
-		cout << "block_4.z: " << block_4.z << endl;
-		cout << " " << endl;
-		}
 
 		// Collision
 		if (IsKeyPressed(KEY_UP))
 		{
-			
-			if (shape == "t")
+			if (shape == 'T')
 			{
 				if (block_1.position == 3)
 				{
@@ -193,36 +175,41 @@ public:
 			}
 
 
-		cout << "---------------------" << endl;
-		cout << "block_1.position: " << block_1.position << endl;
-		cout << "block_1.x: " << block_1.x << endl;
-		cout << "block_1.y: " << block_1.y << endl;
-		cout << "block_1.z: " << block_1.z << endl;
-		cout << " " << endl;
-
-		cout << "block_2.x: " << block_2.x << endl;
-		cout << "block_2.y: " << block_2.y << endl;
-		cout << "block_2.z: " << block_2.z << endl;
-		cout << " " << endl;
-
-		cout << "block_3.position: " << block_3.position << endl;
-		cout << "block_3.x: " << block_3.x << endl;
-		cout << "block_3.y: " << block_3.y << endl;
-		cout << "block_3.z: " << block_3.z << endl;
-		cout << " " << endl;
-
-		cout << "block_4.position: " << block_4.position << endl;
-		cout << "block_4.x: " << block_4.x << endl;
-		cout << "block_4.y: " << block_4.y << endl;
-		cout << "block_4.z: " << block_4.z << endl;
-		cout << " " << endl;
-
 		}
 
 	}
 
-	void Rotate()
-	{}
+	void InitializeTetromino(string blocks) {
+		int num = rand() % blocks.length();
+		char block = blocks[num];
+
+		block = 'T';
+		//{ "T", "S", "L", "I", "S", "Z"}
+		switch (block){
+			case 'T':
+				block_1.x = 0.0f;
+				block_1.y = 0.0f;
+				block_1.z = -1.0f;
+				block_1.position = 0;
+
+				block_2.x = 0.0f;
+				block_2.y = 0.0f;
+				block_2.z = 0.0f;
+				block_2.position = -1;
+
+				block_3.x = 0.0f;
+				block_3.y = 0.0f;
+				block_3.z = 1.0f;
+				block_3.position = 2;
+
+				block_4.x = 0.0f;
+				block_4.y = 1.0f;
+				block_4.z = 0.0f;
+				block_4.position = 1;
+				break;
+		}
+
+	}
 };
 
 Tetromino tetromino;
@@ -255,6 +242,13 @@ int main(void)
 
 		BeginMode3D(camera);
 
+
+		if (!tetromino.tetromino_exist)
+		{
+			tetromino.InitializeTetromino(blocks);
+
+		}
+
 		// Update
 
 		tetromino.Update();
@@ -264,6 +258,8 @@ int main(void)
 		//DrawCube(cubePosition, cube_size, cube_size, cube_size, RED);
 		//DrawCubeWires(cubePosition, 1.0f, 1.0f, 1.0f, MAROON);
 		//DrawCubeWires(cubePosition, cube_size, cube_size, cube_size, MAROON);
+
+		//if ()
 
 		tetromino.Draw();
 
