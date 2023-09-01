103a1cb23aa8227fab61c04ed1d8d901e2eeaf78
rushxpush <rushxpush@gmail.com>
2023-08-31 21:58:42 -0300

    Board walls implemented

--- Board.cpp
+++ Board.cpp
@@ -14,8 +14,21 @@ void Board::Draw(float delta)
 	{
 		//cout << 'i: ' << i << endl;
 
-		DrawCube({ board[i][0], board[i][1], board[i][2] }, width, height, length, GRAY);
-		DrawCubeWires({ board[i][0], board[i][1], board[i][2] }, width, height, length, LIGHTGRAY);
+		if (board[i][1] == 0 || board[i][1] == size_y - 1)
+		{
+			DrawCube({ board[i][0], board[i][1], board[i][2] }, wall_width, height, length, BLUE);
+			DrawCubeWires({ board[i][0], board[i][1], board[i][2] }, wall_width, height, length, LIGHTGRAY);
+		}
+		else if (board[i][2] == 0 || board[i][2] == size_z - 1)
+		{
+			DrawCube({ board[i][0], board[i][1], board[i][2] }, wall_width, height, length, RED);
+			DrawCubeWires({ board[i][0], board[i][1], board[i][2] }, wall_width, height, length, LIGHTGRAY);
+		}
+		else {
+			DrawCube({ board[i][0], board[i][1], board[i][2] }, width, height, length, GRAY);
+			DrawCubeWires({ board[i][0], board[i][1], board[i][2] }, width, height, length, LIGHTGRAY);
+
+		}
 	}
 };
 
--- Board.h
+++ Board.h
@@ -11,6 +11,8 @@ private:
 	float width = 0.2f;
 	float height = 1.0f;
 	float length = 1.0f;
+
+	float wall_width = 1.0f;
 	float x = 1.0f;
 	float y = 0.0f;
 	float z = 0.0f;
@@ -23,9 +25,11 @@ private:
 
 public:
 	// Board size
+	int ceiling = 2;
+	int floor = 2;
 	int size_x = 0;
-	int size_y = 20;
-	int size_z = 10;
+	int size_y = 20 + ceiling; 
+	int size_z = 10 + floor;  
 	void Draw(float delta);
 	void Update(float delta);
 	void GenerateBoard();
